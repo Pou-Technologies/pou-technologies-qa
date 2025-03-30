232a6a3fb2c5e84228154e059a5c505cdd198231
Poutechnologies <poutechnologies@hotmail.com>
2025-03-29 21:32:57 -0300

    Updated version 1.0.4
    Version Updated 1.0.4

--- includes/footer.php
+++ includes/footer.php
@@ -48,7 +48,7 @@
         <!-- Footer Bottom Section -->
         <div class="d-flex flex-column flex-sm-row justify-content-between py-4 border-top">
             <p>© 2025, <span id="colora">Pou Technologies</span>. All Rights Reserved.<br>
-                <span class="text-secondary">version 1.0.3</span>
+                <span class="text-secondary">Version 1.0.4</span>
             </p>
             <ul class="list-unstyled d-flex">
                 <!-- <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-x" style="font-size: 1.5rem;"></i></a></li> -->
@@ -56,8 +56,12 @@
                 <li class="ms-3"><a class="link-body-emphasis" href="https://www.facebook.com/share/15s2bkwjxV/" target="_blank"><i class="bi bi-facebook" style="font-size: 1.5rem;"></i></a></li>
             </ul>
         </div>
+        
     </footer>
+    
 </section>
+
+
 
 
 <!-- Bundle JS (Bootstrap y Popper.js) -->
